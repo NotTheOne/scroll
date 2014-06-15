f8ddb01c840f673460f59ed002552548bb319a32
Roger Camps <rogercampsny@gmail.com>
2014-06-15 05:22:27 -0400

    start styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Scroll::Application.routes.draw do
+  get "homepage/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,7 +14,8 @@ Scroll::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+resources :homepage
+root 'homepage#index'
   # Example resource route with options:
   #   resources :products do
   #     member do
